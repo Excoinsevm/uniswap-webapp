86d75491e17acc17b8d20396ce3206fcf32e0164
Igor Lobanov <igor.lobanov@oodlefinance.com>
2022-06-26 15:42:40 +0000

    added bootstrap and basic UI scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { Navbar, Container, Accordion } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Forked Uniswap v2 by <a href="https://github.com/lobanov/uniswap-webapp">@lobanov</a></Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>List pairs</Accordion.Header>
+          <Accordion.Body>Lorem ipsum</Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Add liquidity</Accordion.Header>
+          <Accordion.Body>Lorem ipsum</Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Exchange</Accordion.Header>
+          <Accordion.Body>Lorem ipsum</Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
   );
 }
 
